d327a8c2af67bf8867eaeb4fd511fb5ae40232bf
Roberto Requena <rart3001@gmail.com>
2016-08-11 18:31:09 -0400

    Initial import to CVS

---
+++ fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/rest/JaxRsActivator.java
@@ -0,0 +1,40 @@
+package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.rest;
+
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.rest.services.*;
+import com.google.common.collect.ImmutableSet;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+/**
+ * The Class <code>com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.rest.JaxRsActivator</code>
+ * <p/>
+ * Created by Roberto Requena - (rart3001@gmail.com) on 13/11/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class JaxRsActivator extends Application {
+
+    private static final ImmutableSet<Class<?>> services = ImmutableSet.of(
+            AvailableNodes.class,
+            HelloResource.class,
+            OnlineComponents.class,
+            Profiles.class,
+            Nodes.class,
+            NetworkData.class,
+            Monitoring.class,
+            UserAuth.class,
+            ConfigurationService.class,
+            DeveloperDatabaseResource.class,
+            DataBases.class,
+            Actors.class
+    );
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return services;
+    }
+
+}
